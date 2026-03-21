4be7a8aafbfee82048687fb1f97828a2c8312cc5
nichigovsky <inichigovsky@gmail.com>
2026-03-21 20:45:22 +0300

    fix: random number

---  2-random-api/main.go
+++  2-random-api/main.go
@@ -9,7 +9,7 @@ import (
 func main(){
 	router := http.NewServeMux()
 	router.HandleFunc("/random", func(w http.ResponseWriter, r *http.Request) {
-		rn := rand.Intn(6)
+		rn := rand.Intn(7)
 
 		w.Write([]byte(fmt.Sprint(rn)))
 		return
@@ -19,7 +19,7 @@ func main(){
 		Addr: ":8081",
 		Handler: router,
 	}
-	
+
 	fmt.Println("Server is listening on port 8081")
 	server.ListenAndServe()
 }
