3e9f39fc45d25f4af487acff7432b5d3ef9c8df7
nichigovsky <inichigovsky@gmail.com>
2026-04-01 15:44:54 +0300

    feat: add main, env.example

--- 4-order-api-start/cmd/main.go
+++ 4-order-api-start/cmd/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"4-order-api/configs"
+	"4-order-api/pkg/db"
+	"fmt"
+	"net/http"
+)
+
+func main(){
+	conf := configs.LoadConfig()
+	_ = db.NewDb(conf)
+	
+	router := http.NewServeMux()
+
+	server := http.Server{
+		Addr: ":8081",
+		Handler: router,
+	}
+
+	fmt.Println("Server is listening on port 8081")
+	server.ListenAndServe()
+}
